fc2c8db2c8ccf7ca498118e941c88638401c6975
Aameen209 <0000aameen@gmail.com>
2025-03-16 17:43:55 +0000

    Comparisons

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,26 @@
+/********Basic Comparisons (>, >=, <, ==, !=)********/
+
+// console.log(2 > 1);   // true  (2 is greater than 1)
+// console.log(2 >= 1);  // true  (2 is greater than or equal to 1)
+// console.log(2 < 1);   // false (2 is not less than 1)
+// console.log(2 == 1);  // false (2 is not equal to 1)
+// console.log(2 != 1);  // true  (2 is not equal to 1)
+
+/******Comparing Strings with Numbers******/
+console.log("2" > 1);   // true  ("2" is converted to number 2, and 2 > 1)
+console.log("02" > 1);  // true  ("02" is converted to 2, and 2 > 1)
+
+/****** / NULL in Comparisons *****/
+// console.log(null > 0); 
+// console.log(null == 0); // Comparison converts null into a number treating it as 0 
+// console.log(null >= 0); // that's why (3)null >= 0 is true and (1)null >0 is false
+
+/****** / UNDEFINED in Comparisons *****/
+//undefined is not converted to 0 and results in false in all cases
+console.log(undefined == 0);  // false (undefined is only equal to null)
+console.log(undefined > 0);   // false (undefined is treated as NaN)
+console.log(undefined < 0);   // false (undefined is treated as NaN)
+
+/****** Strict Equality (===) ******/
+
+// console.log("2" === 2); //checks both value and data-type =>false
